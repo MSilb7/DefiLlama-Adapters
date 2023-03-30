2cae35d21e1bb9bca9a6604ff60394f207eda8cc
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-03-30 10:48:22 -0400

    disclaim

--- projects/treasury/optimism-collective.js
+++ projects/treasury/optimism-collective.js
@@ -1,5 +1,7 @@
 const { nullAddress, treasuryExports } = require("../helper/treasury");
 
+//NOTE: THIS MAY NOT BE CORRECT, JUST PLAYING AROUND WITH STUFF
+
 const treasury = "0x2501c477d0a35545a387aa4a3eee4292a9a8b3f0";
 const vestingAddress = "0x2A82Ae142b2e62Cb7D10b55E323ACB1Cab663a26";
 const OP = "0x4200000000000000000000000000000000000042"; //OP Token
